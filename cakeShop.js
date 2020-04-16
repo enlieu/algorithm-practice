cd24e1866c8d7b7e23e3f334d1266ddde93caccc
Nelson Liu <nelson.ls.liu@gmail.com>
2020-04-15 23:01:50 -0400

    solved cake shop algorithm!

---
+++ cakeShop.js
@@ -0,0 +1,28 @@
+function order(takeout, dineIn, served) {
+  let takeoutIdx = 0;
+  let dineInIdx = 0;
+  let combinedArr = [];
+
+  for (let i = 0; i < served.length; i++) {
+    if (takeout[takeoutIdx] < dineIn[dineInIdx]) {
+      combinedArr.push(takeout[takeoutIdx]);
+      takeoutIdx++;
+    } else {
+      combinedArr.push(dineIn[dineInIdx]);
+      dineInIdx++;
+    }
+  }
+  combinedArr = combinedArr
+    .concat(takeout.slice(takeoutIdx))
+    .concat(dineIn.slice(dineInIdx));
+
+  if (JSON.stringify(combinedArr) == JSON.stringify(served)) {
+    return true;
+  }
+  return false;
+}
+
+order([1, 3, 5], [2, 4, 6], [1, 2, 3, 4, 5, 6]); //true
+order([1, 3, 5, 7], [2, 4, 6], [1, 6, 3, 4, 5, 2]); //false
+order([1, 3, 8], [2, 4, 6, 9], [3, 5, 8, 6, 7, 1]); //false
+order([1, 3, 5, 7], [2, 4, 6, 9], [1, 2, 3, 4, 5, 6, 7, 9]); //true
