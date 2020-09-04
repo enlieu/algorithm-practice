6dbed518e2ceb7c3638db57affe31d84b82d7d28
Nelson Liu <58571687+enlieu@users.noreply.github.com>
2020-09-04 12:22:34 -0400

    84 ms

--- solutions/763. Partition Labels.js
+++ solutions/763. Partition Labels.js
@@ -3,40 +3,40 @@
  * @return {number[]}
  */
 var partitionLabels = function(S) {
-  let ans = [];
-  let hash = {};
-  for (let i = 0; i < S.length; i++) {
-    if (!hash.hasOwnProperty(S[i])) {
-      hash[S[i]] = []
-    }
-    hash[S[i]].push(i)
+  let map = new Map();
+  
+  for (let i = 0; i < S.length; i++) {
+    if (!map.has(S[i])) {
+      map.set(S[i], [i])
+    } 
+    map.get(S[i]).push(i)
   }
-  
-  let startEnd = Object.values(hash).map((idx) => {
-    return [Math.min(...idx), Math.max(...idx)]
-  }).sort((a, b) => a[0] - b[0])
-  
-  
-  let currentInterval = startEnd[0]
-  let outputArr = [startEnd[0]];
-  
-   for (let i = 1; i < startEnd.length; i++) {
-     let currentStart = currentInterval[0]
-     let currentEnd = currentInterval[1]
-     let nextStart = startEnd[i][0]
-     let nextEnd = startEnd[i][1]
-     
-     if (currentEnd >= nextStart) {
-       currentInterval[1] = Math.max(currentEnd, nextEnd)
-     } else {
-       currentInterval = startEnd[i]
-       outputArr.push(currentInterval)
-     }
-   }
-
-  for (let i = 0; i < outputArr.length; i++) {
-    ans.push(outputArr[i][1] - outputArr[i][0] + 1)
+  
+  let idxs = Array.from(map.values())
+  
+  for (let i = 0; i < idxs.length; i++) {
+    let firstIndex = idxs[i][0]
+    let lastIndex = idxs[i][idxs[i].length-1]
+    
+    idxs[i] = [firstIndex, lastIndex]
   }
-  
-  return ans;
+​
+  let toCompare = idxs[0]
+  let res = [toCompare]
+  
+  for (let i = 1; i < idxs.length; i++) {
+    if (idxs[i][0] < toCompare[1]) {
+      toCompare[1] = Math.max(toCompare[1], idxs[i][1])
+    } else {
+      res.push(idxs[i])
+      toCompare = idxs[i]
+    }
+  }
+  
+  for (let i = 0; i < res.length; i++) {
+    res[i] = res[i][1] - res[i][0] + 1
+  }
+  
+  return res
+​
 };
