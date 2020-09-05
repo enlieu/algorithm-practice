34b372489357ce5c962f194346d9f70cc20454f8
Nelson Liu <58571687+enlieu@users.noreply.github.com>
2020-09-05 00:34:45 -0400

    116 ms

--- solutions/912. Sort an Array.js
+++ solutions/912. Sort an Array.js
@@ -3,5 +3,37 @@
  * @return {number[]}
  */
 var sortArray = function(nums) {
-    return nums.sort((a, b) => a - b)
+  const merge = (arr1, arr2) => {
+    let i = 0;
+    let j = 0;
+    let res = [];
+    while (i < arr1.length && j < arr2.length) {
+      if (arr1[i] < arr2[j]) {
+        res.push(arr1[i])
+        i++
+      } else {
+        res.push(arr2[j])
+        j++
+      }
+    }
+    
+    while (i < arr1.length) {
+      res.push(arr1[i])
+      i++
+    }
+    
+    while (j < arr2.length) {
+      res.push(arr2[j])
+      j++
+    }
+    return res;
+  }
+  
+  if (nums.length <= 1) return nums
+  
+  let mid = Math.floor(nums.length/2)
+  let left = sortArray(nums.slice(0, mid))
+  let right = sortArray(nums.slice(mid))
+  
+  return merge(left, right)
 };
