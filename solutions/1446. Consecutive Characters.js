e6adbb12593f74f5ef315360fe960fa677bc9668
Nelson Liu <58571687+enlieu@users.noreply.github.com>
2020-11-04 13:16:32 -0500

    80 ms

--- solutions/1446. Consecutive Characters.js
+++ solutions/1446. Consecutive Characters.js
@@ -3,24 +3,23 @@
  * @return {number}
  */
 var maxPower = function(s) {
-  if (s.length === 1) return 1
-  
-  let left = 0;
-  let right = 1;
-  let maxPower = -Infinity
-  let insidePower = 0;
-  
-  
-  
-  while (right < s.length) {
-    if (s[left] === s[right]) {
-      right++ 
-      insidePower = right - left
-    } else {
-      left++
-    }
-    maxPower = Math.max(maxPower, insidePower)
+  if (!s.length) return 0
+  
+  let left = 0;
+  let right = 1;
+  let power = 1;
+  let maxPower = 1;
+​
+  while (right < s.length) {
+    if (s[left] === s[right]) {
+      power++
+    } else {
+      power = 1;
+      left = right
+    }
+    maxPower = Math.max(power, maxPower)
+    right++
   }
-  return maxPower
-  
+  
+  return maxPower;  
 };
