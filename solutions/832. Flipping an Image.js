2d5065a8cf5d420d528c80498cebb03038ce3a62
Nelson Liu <58571687+enlieu@users.noreply.github.com>
2020-11-10 11:06:50 -0500

    84 ms

--- solutions/832. Flipping an Image.js
+++ solutions/832. Flipping an Image.js
@@ -3,7 +3,11 @@
  * @return {number[][]}
  */
 var flipAndInvertImage = function(A) {
-  let inverted = A.map((image) => image.reverse().map((el) => el === 0 ? 1 : 0))
-  
-  return inverted
+  for (const row of A) {
+    row.reverse()
+    for (let i = 0; i < row.length; i++) {
+      row[i] === 0 ? row[i] = 1 : row[i] = 0
+    }
+  }
+  return A 
 };
